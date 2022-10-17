57645cb9656f8888ac92b2e4ac6074bbe147bcf2
canasmh <canasmh@yahoo.com>
2022-10-17 17:47:12 -0400

    Ensure form is properly running by submitting form and make sure form contents is displayed on page

--- user-onboarding/cypress/e2e/form_test.cy.js
+++ user-onboarding/cypress/e2e/form_test.cy.js
@@ -8,5 +8,39 @@ describe('Form Test', () => {
         cy.get("#fName")
             .type("Manuel")
             .should("have.value", "Manuel");
+        
+    })
+
+    it("Test last name", () => {
+        cy.get("#lName")
+            .type("Canas")
+            .should("have.value", "Canas");
+        
+    })
+
+    it("Test email", () => {
+        cy.get("#email")
+            .type("canasmh@nmsu.edu")
+            .should("have.value", "canasmh@nmsu.edu");
+    })
+
+    it("Test password", () => {
+        cy.get("#password")
+            .type("this-is_my-elaborate&password")
+            .should("have.value", "this-is_my-elaborate&password");
+    })
+
+    it("Test Agreement", () => {
+        cy.get("#agree")
+            .click().should("be.checked")
+    })
+
+    it("Test submit button", () => {
+        cy.contains("Submit")
+            .click();
+    })
+
+    it("Test submission", () => {
+        cy.contains("Manuel Canas")
     })
 });
